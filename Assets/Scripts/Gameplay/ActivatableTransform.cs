443de5247b79ec19cc06d64a15733036f0fbd074
Anton <apiskareff@gmail.com>
2016-12-17 16:30:31 +0400

    Added door. Doesn't close correctly, needs fixing.

---
+++ Assets/Scripts/Gameplay/ActivatableTransform.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ActivatorProxy))]
+public class ActivatableTransform : MonoBehaviour
+{
+    public Vector3 DeltaPosition = Vector3.zero;
+	[Header("Door Settings")]
+	[Tooltip("Скорость активации")]
+	public float ActivationSpeed = 2f;
+	[Tooltip("Скорость деактивации")]
+    public float DeactivationSpeed = 2f;
+	[Tooltip("Изначальный угол")]
+	public float InitialAngle = 0f;
+	[Tooltip("Искомый угол")]
+	public float TargetAngle = 90f;
+	private float ActualAngle = 0f;
+
+    private float _state = 0f;
+	private ActivatorProxy _activator;
+//    private Vector3 _initialPosition;
+//    private Vector3 _targedPosition;
+	//[Tooltip("Настройки оси вращения")]
+	[Header("Hinge settings")]
+	[Tooltip("Центр оси вращения")]
+	public Vector3 HingePoint;
+	[Tooltip("Направление оси вращения")]
+	public Vector3 HingeDirection = Vector3.up;
+
+    void Awake()
+    {
+     //   _initialPosition = transform.position;
+      //  _targedPosition = _initialPosition + DeltaPosition;
+    }
+
+    void Start ()
+    {
+        _activator = GetComponent<ActivatorProxy>();
+    }
+	
+	void Update ()
+    {
+		if (_activator.IsActivated && ActualAngle < TargetAngle)
+	    {
+			transform.RotateAround(transform.TransformPoint(HingePoint), transform.TransformDirection(HingeDirection), ActivationSpeed);
+			Debug.LogWarning ("ActivatedOpen");
+			ActualAngle+=ActivationSpeed;
+		}
+
+		/*if (_activator.IsActivated && ActualAngle >= TargetAngle)
+		{
+			transform.RotateAround(transform.TransformPoint(HingePoint), transform.TransformDirection(HingeDirection), ActivationSpeed);
+			Debug.LogWarning ("ActivatedClose");
+			ActualAngle-=ActivationSpeed;}*/
+	}
+
+	public void OnDrawGizmosSelected()
+	{
+		Gizmos.DrawLine(transform.TransformPoint(HingePoint),
+			transform.TransformPoint(HingePoint) + transform.TransformDirection(HingeDirection));
+		Gizmos.DrawWireCube(transform.TransformPoint(HingePoint), Vector3.one * 0.2f); 
+	}
+}
---
+++ Assets/Scripts/Gameplay/ActivateArea.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class ActivateArea : MonoBehaviour
+{
+	public GameObject[] Targets;
+
+	//private int _playersTriggered = 0;
+
+	private float _currentState = 0f;
+	private Vector3 _initialPosition;
+	private Vector3 _pressedPosition;
+
+	string message = "";
+	bool InTrigger = false;
+	public bool IsActivated { get; private set; }
+
+	void Start ()
+	{
+		//_initialPosition = transform.position;
+		//_pressedPosition = transform.position + Vector3.up*PressDepth;
+		IsActivated = false;
+
+	//		if (!GetComponent<Collider>().isTrigger)
+	//			Debug.LogWarning("Button collider must be a trigger", this);
+	}
+
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Activate") && InTrigger == true) {
+			//Debug.LogWarning ("E pressed");
+			IsActivated = true;
+			foreach (var target in Targets) {
+				target.SendMessage (ActivatorProxy.ActivateEvent);
+				}
+			}
+			
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.LogWarning ("Trigger entered", this);
+		message = "Press E to open";
+		InTrigger = true;
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		Debug.LogWarning ("Trigger exited", this);
+		message = "";
+		InTrigger = false;
+	}
+
+	void OnGUI(){
+		GUI.Label (new Rect (300, 300, 200, 200), message);
+	}
+
+		
+}
