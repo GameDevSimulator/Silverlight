0d274b21fb6b2583d571d771e071bcf2cce452bb
Boris shishov <borisshishov@gmail.com>
2016-12-18 23:08:01 +0300

    Darkness DX11 sampling

--- Assets/Scripts/Gameplay/Darkness/Darkness.cs
+++ Assets/Scripts/Gameplay/Darkness/Darkness.cs
@@ -7,7 +7,10 @@ namespace Assets.Scripts.Gameplay.Darkness
     [RequireComponent(typeof(MeshRenderer))]
     public class Darkness : MonoBehaviour
     {
+        public ComputeShader SamplingComputeShader;
         public Material ComputationalMaterial;
+        public Texture InitialStateTexture;
+        public Texture ShapeTexture;
         public float AngularDamping = 0.5f;
 
         [Range(1f, 20f)]
@@ -27,22 +30,44 @@ namespace Assets.Scripts.Gameplay.Darkness
         private Rect _state2DRect;
         private bool _update = false;
 
+       
+
+        private const string StateTextureName = "_StateTex";
+        private const string MaskTextureName = "_MaskTex";
+        private const int RenderTextureDepth = 0;
+        private const RenderTextureFormat RenderTextureFormat = UnityEngine.RenderTextureFormat.ARGB32;
+
+
+
+        // COMPUTE SHADER SAMPLING //
+
+        /// <summary>
+        /// Structure of sample, must be eq to compute shade struct
+        /// </summary>
+        struct Sample
+        {
+            public Vector2 At;
+            public float Value;
+        }
+        private bool _useComputeSampling; // Shader model 5.0 support (DX11)
+        private const string ComputeShaderSamplingKernel = "CSSampling"; // Name of the kernel in CS
+        private const string ComputeShaderSamplingStateTextue = "StateTex"; // Name of state texture in CS
+        private const string ComputeShaderSamplingStateBuffere = "SampleBuffer"; // Name of Sample buffer in CS
+        private const int ComputeShaderSamplingBufferSize = 50; // Also check DEFINE in compute shader
+        private const int ComputeShaderStructSize = sizeof(float) * 3; // size of Sample struct
+        private readonly Sample[] _gpuSamples = new Sample[ComputeShaderSamplingBufferSize]; // Input buffer
+        private readonly Sample[] _gpuSamplesOut = new Sample[ComputeShaderSamplingBufferSize]; // Output buffer
+
 #if DEBUG
+        // Output rectangle for displaying debug state
         private readonly Rect _rect = new Rect(0, 0, 256, 256);
 #endif
-    
-        const string StateTextureName = "_StateTex";
-        const int RenderTextureDepth = 0;
-        const RenderTextureFormat RenderTextureFormat = UnityEngine.RenderTextureFormat.ARGB32;
-
-        void Awake()
-        {
-            if(_materialInstance == null)
-                _materialInstance = Instantiate(ComputationalMaterial);
-        }
 
         void Start ()
         {
+            if (_materialInstance == null)
+                _materialInstance = Instantiate(ComputationalMaterial);
+
             //_meshFilter = GetComponent<MeshFilter>();
             _meshRenderer = GetComponent<MeshRenderer>();
 
@@ -52,12 +77,26 @@ namespace Assets.Scripts.Gameplay.Darkness
             _bufferRenderTexture = new RenderTexture(512, 512, RenderTextureDepth, RenderTextureFormat);
             _bufferRenderTexture.Create();
 
-            Graphics.Blit(ComputationalMaterial.mainTexture, _stateRenderTexture);
+            // Set default state
+            Graphics.Blit(InitialStateTexture ?? ComputationalMaterial.mainTexture, _stateRenderTexture);
 
-            _state2D = new Texture2D(_stateRenderTexture.width, _stateRenderTexture.height);
+            // Set mask
+            _materialInstance.SetTexture(MaskTextureName, ShapeTexture);
+
+            _state2D = new Texture2D(_stateRenderTexture.width, _stateRenderTexture.height)
+            {
+                wrapMode = TextureWrapMode.Clamp,
+                filterMode = FilterMode.Point
+            };
             _state2DRect = new Rect(0, 0, _state2D.width, _state2D.height);
 
-            //_interactors.Add(GameManager.Instance.Player.GetComponentInChildren<DarknessInteractor>());
+          
+            _useComputeSampling = SystemInfo.graphicsShaderLevel >= 50;
+
+            if (SamplingComputeShader == null && _useComputeSampling)
+                Debug.LogError("Missing sampling compute shader");
+
+            // Always track players flashlight
             var fl = GameManager.Instance.Player.GetComponentInChildren<FlashLight>();
             _interactors.Add(fl.GetComponent<DarknessInteractor>());
         }
@@ -71,6 +110,16 @@ namespace Assets.Scripts.Gameplay.Darkness
             {
                 RenderState(_stateRenderTexture, _bufferRenderTexture);
 
+                if (_useComputeSampling)
+                {
+                    GpuSampling();
+                }
+                else
+                {
+                    _state2D.ReadPixels(_state2DRect, 0, 0, false);
+                    _state2D.Apply();
+                }
+
                 // Proccess darkness
                 Graphics.Blit(_bufferRenderTexture, _stateRenderTexture, _materialInstance, 0);
 
@@ -78,20 +127,27 @@ namespace Assets.Scripts.Gameplay.Darkness
                 _meshRenderer.material.SetTexture(StateTextureName, _bufferRenderTexture);
             }
 
-
+            var interactorSampleId = 1;
             foreach (var interactor in _interactors)
             {
                 var body = interactor.GetComponent<Rigidbody>();
                 if (body != null)
                 {
                     var forceFactorSum = 0f;
+                    var idx = 0;
                     
                     foreach (var sample in interactor.Samples)
                     {
+                        if (_useComputeSampling)
+                            QueueSample(interactorSampleId + idx, interactor.transform.TransformPoint(sample.LocalPosition));
+
                         forceFactorSum += CalcForceFactor(
+                            interactorSampleId + idx,
                             interactor.transform.TransformPoint(sample.LocalPosition),
                             interactor.transform.TransformDirection(-sample.Direction.normalized),
                             Vector3.up);
+
+                        idx++;
                     }
                     
                     forceFactorSum = Mathf.Max(forceFactorSum, 1f);
@@ -99,19 +155,25 @@ namespace Assets.Scripts.Gameplay.Darkness
                     if (forceFactorSum > 0)
                     {
                         var forceSum = 0f;
+
+                        idx = 0;
                         foreach (var sample in interactor.Samples)
                         {
                             var factor = CalcForceFactor(
+                                interactorSampleId + idx,
                                 interactor.transform.TransformPoint(sample.LocalPosition),
                                 interactor.transform.TransformDirection(-sample.Direction.normalized),
                                 Vector3.up);
 
-                            forceSum += SamplePhysicsAt(
+                            forceSum += SamplePhysicsAt
+                                (interactorSampleId + idx,
                                 interactor.transform.TransformPoint(sample.LocalPosition), 
                                 body,
                                 factor / forceFactorSum,
                                 1f / interactor.Samples.Length
                             );
+
+                            idx++;
                         }
 
                         if (forceSum > 0.1f)
@@ -129,6 +191,8 @@ namespace Assets.Scripts.Gameplay.Darkness
                         }
 #endif
                     }
+
+                    interactorSampleId += idx;
                 }
             }
         }
@@ -178,10 +242,8 @@ namespace Assets.Scripts.Gameplay.Darkness
             }
             GL.PopMatrix();
 
-            // TODO: OPTIMIZE PERFORMANCE BY C++ PLUGIN OR COMPUTE SHADERS
-            // TODO: REMOVE PIPELINE STALLING
-            _state2D.ReadPixels(_state2DRect, 0, 0, false);
-            _state2D.Apply();
+
+          
         }
 
         private void RenderMesh(Transform t, Mesh mesh, DarknessInteractor.ProcessingMode mode)
@@ -190,18 +252,13 @@ namespace Assets.Scripts.Gameplay.Darkness
             foreach (var index in mesh.triangles)
             {
                 var v = mesh.vertices[index];
-                v = World2GL(t.TransformPoint(v));
+                v = WorldToUv(t.TransformPoint(v));
                 //GL.TexCoord(mesh.uv[mesh.triangles[i]]);
                 if(mode == DarknessInteractor.ProcessingMode.MeshWithColorData && mesh.colors.Length > 0)
                     GL.Color(mesh.colors32[index]);
                 GL.Vertex3(v.x, v.y, v.z + 0.1f);
             }
             GL.End();
-        }
-
-        private Vector3 World2GL(Vector3 position)
-        {
-            return transform.InverseTransformPoint(position) + new Vector3(0.5f, 0.5f);
         }
 
         void OnGUI()
@@ -213,7 +270,6 @@ namespace Assets.Scripts.Gameplay.Darkness
             }
 #endif
         }
-
 
         void OnTriggerEnter(Collider col)
         {
@@ -246,15 +302,15 @@ namespace Assets.Scripts.Gameplay.Darkness
             return col.bounds.min;
         }
 
-        private float CalcForceFactor(Vector3 at, Vector3 objectSurfaceNormal, Vector3 forceDirection)
+        private float CalcForceFactor(int sampleId, Vector3 at, Vector3 objectSurfaceNormal, Vector3 forceDirection)
         {
-            return Mathf.Max(Vector3.Dot(forceDirection, objectSurfaceNormal), 0) * GetState(at);
+            return Mathf.Max(Vector3.Dot(forceDirection, objectSurfaceNormal), 0) * GetState(at, sampleId);
         }
 
-        private float SamplePhysicsAt(Vector3 sample, Rigidbody body, float gravityFactor, float frictionFactor)
+        private float SamplePhysicsAt(int sampleId, Vector3 sample, Rigidbody body, float gravityFactor, float frictionFactor)
         {
             const float threshold = 0.1f;
-            var state = GetState(sample);
+            var state = GetState(sample, sampleId);
 
             if (state > threshold)
             {
@@ -302,17 +358,33 @@ namespace Assets.Scripts.Gameplay.Darkness
             return (a + b) * 0.5f;
         }
 
-        private float GetState(Vector3 worldPosition)
+        private float GetState(Vector3 worldPosition, int sampleId = 0)
         {
-            var point = transform.InverseTransformPoint(worldPosition) + new Vector3(0.5f, 0.5f);
+            if (_useComputeSampling)
+                return DequeueSample(sampleId);
 
-            if (point.x < 0 || point.x > 1)
+            var point = WorldToUv(worldPosition);
+            return _state2D.GetPixel((int)(point.x * _state2D.width), (int)(point.y * _state2D.height)).r;
+        }
+
+        public Vector2 WorldToUv(Vector3 worldPosition)
+        {
+            return transform.InverseTransformPoint(worldPosition) + new Vector3(0.5f, 0.5f);
+        }
+
+        private void QueueSample(int id, Vector3 position)
+        {
+            if (id > ComputeShaderSamplingBufferSize - 1)
+                return;
+
+            _gpuSamples[id].At = WorldToUv(position);
+        }
+
+        private float DequeueSample(int id)
+        {
+            if (id > ComputeShaderSamplingBufferSize - 1)
                 return 0;
-
-            if (point.y < 0 || point.y > 1)
-                return 0;
-
-            return _state2D.GetPixel((int)(point.x *_state2D.width), (int)(point.y * _state2D.height)).r;
+            return _gpuSamplesOut[id].Value;
         }
 
         public bool OnBeamRayHit(RaycastHit hit, Vector3 rdir, float maxDistance, out Vector3 endPoint)
@@ -335,6 +407,20 @@ namespace Assets.Scripts.Gameplay.Darkness
 
             endPoint = Vector3.zero;
             return false;
+        }
+
+        private void GpuSampling()
+        {
+            var kernel = SamplingComputeShader.FindKernel(ComputeShaderSamplingKernel);
+            var buffer = new ComputeBuffer(ComputeShaderSamplingBufferSize, ComputeShaderStructSize);
+            buffer.SetData(_gpuSamples);
+
+            SamplingComputeShader.SetTexture(kernel, ComputeShaderSamplingStateTextue, _bufferRenderTexture);
+            SamplingComputeShader.SetBuffer(kernel, ComputeShaderSamplingStateBuffere, buffer);
+            SamplingComputeShader.Dispatch(kernel, 2, 1, 1);
+            
+            buffer.GetData(_gpuSamplesOut);
+            buffer.Release();
         }
     }
 }
